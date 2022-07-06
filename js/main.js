4e47a24aba694ac710245abb1bcc6cd2462fd97a
Максим Дзюбенко <maksim.dzjubenko@ivkhk.ee>
2022-07-06 21:18:58 +0300

    Made the note adding.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 const App = {
     data() {
         return {
-            message: 'Hello Vue!'
+            message: 'Hello Vue!',
+            inputValue: '',
+            notes: []
+        }
+    },
+    'methods': {
+        inputChangeHandler(event) {
+            this.inputValue = event.target.value
+        },
+        addNote() {
+            this.notes.push(this.inputValue);
+            this.inputValue = '';
         }
     }
 }
-Vue.createApp(App).mount('h1')
+Vue.createApp(App).mount('#app'); // set values into html file

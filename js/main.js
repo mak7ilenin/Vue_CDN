210ede6764dbe6a91b8a847a19fba8f80cf984fa
Максим Дзюбенко <maksim.dzjubenko@ivkhk.ee>
2022-07-06 23:26:40 +0300

    Made a remove button (without logic yet).

--- js/main.js
+++ js/main.js
@@ -1,19 +1,48 @@
+
 const App = {
     data() {
         return {
             message: 'Hello Vue!',
             inputValue: '',
+            inputClasses: '',
+            removeBtnClasses: '',
+            placeholderName: 'Enter the note title',
             notes: []
         }
     },
     'methods': {
-        inputChangeHandler(event) {
+        inputValueHandler(event) {
             this.inputValue = event.target.value
+            if(this.notes.length < 1) {
+                this.removeBtnClasses = 'disabled';
+            } else {
+                this.removeBtnClasses = '';
+            }
         },
         addNote() {
-            this.notes.push(this.inputValue);
-            this.inputValue = '';
+            if(this.inputValue === '') {
+                this.placeholderName = 'Can not add empty field!';
+                this.inputClasses = 'red_input';
+                return;
+            }else {
+                this.inputClasses = '';
+                this.placeholderName = 'Enter the note title';
+                this.notes.push(this.inputValue);
+                this.inputValue = '';
+                if(this.notes.length > 0) {
+                    this.removeBtnClasses = '';
+                }
+            }
+        },
+        keyPressHandler(event) {
+            if(event.key === 'Enter') {
+                this.addNote();
+            }
+        },
+        removeNote() {
+            
         }
     }
+
 }
-Vue.createApp(App).mount('#app'); // set values into html file
+Vue.createApp(App).mount('#app');    // set values into html file
